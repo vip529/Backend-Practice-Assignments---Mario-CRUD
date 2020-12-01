4d8629a64b9478e7e53990ccc3fa024a68ef64e2
vip529 <srvip028@gmail.com>
2020-12-02 01:53:48 +0530

    nn1

--- src/app.js
+++ src/app.js
@@ -14,5 +14,69 @@ app.use(bodyParser.json())
 // your code goes here
 
 
+app.get('/mario',(req,res)=>{
+    marioModel.find({})
+    .then((result)=>{
+        res.status(200).json(result);
+    })
+    .catch((error)=>{
+        res.status(400).json({"message": error.message});
+    })
+})
+
+app.get('/mario/:id',(req,res)=>{
+    const id = req.params.id;
+    marioModel.findById(id)
+    .then((result) => {
+        res.status(200).json(result);
+    })
+    .catch((error)=>{
+        res.status(400).json({"message": error.message});
+    })
+
+})
+
+
+app.post('/mario',(req,res)=>{
+    const {name,weight} = req.body;
+    let newMario =new marioModel({
+        name : name,
+        weight: weight
+    })
+    newMario.save()
+    .then((result)=>{
+        res.status(201).json(result);
+    })
+    .catch((error)=>{
+        res.status(400).json({"message": 'either name or weight is missing'});
+    })
+})
+
+app.patch('/mario/:id',(req,res)=>{
+    const id = req.params.id;
+    const updates = req.body;
+    console.log(updates)
+    marioModel.findByIdAndUpdate(id,updates)
+    .then((result)=>{
+        res.status(200).json(result);
+    })
+    .catch((error)=>{
+        res.status(400).json({"message": error.message});
+    })
+
+})
+
+app.delete('/mario/:id',(req,res) =>{
+    const id = req.params.id;
+    marioModel.findByIdAndDelete(id)
+    .then((result)=>{
+        res.status(200).json({"message": 'character deleted'});
+    })
+    .catch((error)=>{
+        res.status(400).json({"message": error.message});
+    })
+})
+
+
 
 module.exports = app;
--- src/models/marioChar.js
+++ src/models/marioChar.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 
-//  Your code goes here
-
+const marioSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    weight:{
+        type: Number,
+        required: true,
+    }
+})
+const marioModel = mongoose.model('marioModel',marioSchema);
 
 module.exports = marioModel;
